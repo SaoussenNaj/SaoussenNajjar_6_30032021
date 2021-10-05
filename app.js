0d20405e7e7053906c8adbaccbf0261d78a39ba1
Saoussen Najjar <saoussen.najjar110@gmail.com>
2021-10-05 23:12:33 +0200

    second commit

--- app.js
+++ app.js
@@ -2,11 +2,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-// creation variable app pour notre application et appeler express()ce qui permet de creer une application express
-// Ajout d'un middleware et our passer à un autre on utilise next()
-const app = express();
 const userRoutes = require("./routes/user");
+// creation variable app pour notre application et appeler express()ce qui permet de creer une application express
+const app = express();
+
 // Etablir la connexion avec la BD
 mongoose
   .connect(
@@ -16,7 +15,8 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log(error));
 
-// middelware general appliqué a toytes les routes du serveur
+// middelware general appliqué a toutes les routes du serveur
+// pour passer d'un middelware à un autre on utilise next()
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -32,5 +32,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use("/api/auth", userRoutes);
+
 // exporter cette const app pour qu'on puisse y acceder depuis les autres fichiers
 module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,6 @@
-const User = require("../models/User");
 const bcrypt = require("bcrypt");
-
-// BCRYPT password.
-// Récupérer l'email et le mot de pass hasher pour l'envoyer à MonGoDB
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
   bcrypt
@@ -20,4 +18,34 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+// // BCRYPT password.
+// // Récupérer l'email et le mot de pass hasher pour l'envoyer à MonGoDB
+
+exports.login = (req, res, next) => {
+  exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({ error: "Utilisateur non trouvé !" });
+        }
+        // on compare le mdp entré par l'utilisateur avec le hash enregistré dans la BD
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              return res
+                .status(401)
+                .json({ error: "Mot de passe incorrect !" });
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                expiresIn: "24h",
+              }),
+            });
+          })
+          .catch((error) => res.status(500).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  };
+};
